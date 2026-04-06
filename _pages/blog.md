72a46e76a23802d321a17d8082d1390d58953e3a
Tom Napier <tomjnapier@gmail.com>
2026-04-06 17:06:57 +0100

    fix(tidy): add js minify to build process and tidy up files:

--- _pages/blog.md
+++ _pages/blog.md
@@ -1,5 +1,4 @@
 ---
-layout: page
 title: Articles
 permalink: /blog/
 layout: blog
---
+++ assets/js/dist/app.js
@@ -0,0 +1,1 @@
+import e from"./theme-switcher.js";document.addEventListener("DOMContentLoaded",function(){new e().init()});
